d7fea540acbea23fe9de066696188a7676789a45
Azee <aziztitu1996@gmail.com>
2020-05-03 16:18:35 -0500

    Implements prediction from web interface

--- server/api/views.py
+++ server/api/views.py
@@ -8,6 +8,8 @@ import numpy as np
 import json
 
 # Create your views here.
+
+
 def index(request: HttpRequest):
 
     result = {
@@ -17,49 +19,62 @@ def index(request: HttpRequest):
     }
 
     classifierModels = {
-      'lr': 'lr_classifier.model',
-      'nb': 'nb_classifier.model',
-      'rf': 'rf_classifier.model',
+        'lr': 'lr_classifier.model',
+        'nb': 'nb_classifier.model',
+        'rf': 'rf_classifier.model',
     }
 
-    selectedClassifier = request.GET['classifier']
+    try:
+        selectedClassifier = request.GET['classifier']
 
-    if selectedClassifier not in classifierModels:
-        return HttpResponse(json.dumps(result))
+        if selectedClassifier not in classifierModels:
+            result['success'] = False
+            result['msg'] = 'Invalid Classifier'
+            return HttpResponse(json.dumps(result))
 
-    exportedModelFilePath = path.abspath(path.dirname(
-        __name__)) + '/../exportedModels/' + classifierModels[selectedClassifier]
-    # print(exportMetaDataFilePath)
+        exportedModelFilePath = path.abspath(path.dirname(
+            __name__)) + '/../exportedModels/' + classifierModels[selectedClassifier]
+        # print(exportMetaDataFilePath)
 
-    X = pd.DataFrame({
-        'home_encoded': request.GET['homeTeam'],
-        'away_encoded': request.GET['awayTeam'],
-        'HTHG': request.GET['homeGoals'],
-        'HTAG': request.GET['awayGoals'],
-        'HS': request.GET['homeShots'],
-        'AS': request.GET['awayShots'],
-        'HST': request.GET['homeShotsOnTarget'],
-        'AST': request.GET['awayShotsOnTarget'],
-        'HR': request.GET['homeRedCards'],
-        'AR': request.GET['awayRedCards'],
-    }, index = [0])
+        X = pd.DataFrame({
+            'home_encoded': request.GET['homeTeam'],
+            'away_encoded': request.GET['awayTeam'],
+            'HTHG': request.GET['homeGoals'],
+            'HTAG': request.GET['awayGoals'],
+            'HS': request.GET['homeShots'],
+            'AS': request.GET['awayShots'],
+            'HST': request.GET['homeShotsOnTarget'],
+            'AST': request.GET['awayShotsOnTarget'],
+            'HR': request.GET['homeRedCards'],
+            'AR': request.GET['awayRedCards'],
+        }, index=[0])
 
-    if path.exists(exportedModelFilePath):
-        clf = load(exportedModelFilePath)
-        # print(exportMetaData)
+        if path.exists(exportedModelFilePath):
+            clf = load(exportedModelFilePath)
+            # print(exportMetaData)
 
-        start = time()
-        pred_probs = clf.predict_proba(X)[0]
+            start = time()
+            pred_probs = clf.predict_proba(X)[0]
 
-        y = dict(zip(clf.classes_, pred_probs))
-        end = time()
-        print("Made Predictions in {:2f} seconds".format(end-start))
+            y = dict(zip(clf.classes_, pred_probs))
+            end = time()
+            print("Made Predictions in {:2f} seconds".format(end-start))
 
-        print("Result: ")
-        print(y)
+            print("Result: ")
+            print(y)
 
-        result['data']['homeWin'] = y['H']
-        result['data']['draw'] = y['D']
-        result['data']['awayWin'] = y['A']
+            result['data']['homeWin'] = y['H']
+            result['data']['draw'] = y['D']
+            result['data']['awayWin'] = y['A']
+
+            result['success'] = True
+            result['msg'] = 'Match Result Predicted'
+        else:
+            result['success'] = False
+            result['msg'] = 'Classifier not found'
+
+    except:
+        result['success'] = False
+        result['msg'] = 'Please check your input'
 
     return JsonResponse(result)
