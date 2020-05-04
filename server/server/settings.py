77daeeeff6b1f98998f0785a740dfa7f7fd2c049
Azee <aziztitu1996@gmail.com>
2020-05-04 00:10:46 -0500

    Changes static url prefix

--- server/server/settings.py
+++ server/server/settings.py
@@ -119,4 +119,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/football_match_predictor/static/'
