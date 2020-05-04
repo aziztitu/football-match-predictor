d758afa6c80da768b7b6b374910197ad4764989e
Azee <aziztitu1996@gmail.com>
2020-05-04 00:29:17 -0500

    Adds static root

--- server/server/settings.py
+++ server/server/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/football_match_predictor/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_URL = '/static/'
